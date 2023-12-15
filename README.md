6bbe0012543c15d3d60dc3138084f2d9169c0d04
ghost <localhost>
2023-12-15 20:28:20 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # dns-api
+
 Simple DNS API
+
+## Install
+
+```
+git clone https://github.com/YGGverse/dns-api.git
+cd dns-api
+composer install
+```
+
+## Run
+
+```
+cd src/public
+php -S localhost:8080
+```
+
+## Usage
+
+### Dig
+
+#### Single record
+
+```
+dig.php?name=php.net&record=A
+```
+
+#### Multiple records
+
+```
+dig.php?name=php.net&records[]=A&records[]=AAAA
+```
---
+++ src/public/dig.php
@@ -0,0 +1,92 @@
+<?php
+
+// Set headers
+header('Content-Type: application/json; charset=utf-8');
+
+// Load dependencies
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+// Valid name required to continue
+if (empty($_GET['name']) || !is_string($_GET['name']) || !\Yggverse\Dns\Dig::isHostName($_GET['name']))
+{
+    exit(
+        json_encode(
+            [
+                'success' => false,
+                'message' => _('valid name required')
+            ]
+        )
+    );
+}
+
+// Valid records required to continue
+$records = [];
+
+if (isset($_GET['record']) && is_string($_GET['record']) && \Yggverse\Dns\Dig::isRecord($_GET['record']))
+{
+    $records[] = $_GET['record'];
+}
+
+if (isset($_GET['records']) && is_array($_GET['records']))
+{
+    foreach ($_GET['records'] as $record)
+    {
+        if (is_string($record) && \Yggverse\Dns\Dig::isRecord($record))
+        {
+            $records[] = $record;
+        }
+    }
+}
+
+// At least one record required
+if (empty($records))
+{
+    exit(
+        json_encode(
+            [
+                'success' => false,
+                'message' => sprintf(
+                    _('valid record(s) required: %s'),
+                    implode(
+                        ',',
+                        array_unique(
+                            $records
+                        )
+                    )
+                )
+            ]
+        )
+    );
+}
+
+// Resolve begin
+if (!$result = \Yggverse\Dns\Dig::records($_GET['name'], array_unique($records)))
+{
+    exit(
+        json_encode(
+            [
+                'success' => false,
+                'message' => sprintf(
+                    _('%s records for %s not found'),
+                    implode(
+                        ',',
+                        array_unique(
+                            $records
+                        )
+                    ),
+                    $_GET['name']
+                )
+            ]
+        )
+    );
+}
+
+// Done
+exit(
+    json_encode(
+        [
+            'success' => true,
+            'records' => $result
+        ]
+    )
+);
