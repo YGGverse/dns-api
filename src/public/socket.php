0e89d599c9f6e30633a25c54435e338cb308184c
ghost <localhost>
2023-12-16 16:19:04 +0200

    add REMOTE_ADDR if host not provided, apply timeout

--- src/public/socket.php
+++ src/public/socket.php
@@ -33,11 +33,22 @@ if (isset($_GET['host']) && !\Yggverse\Net\Socket::isHost($_GET['host']))
     );
 }
 
+// Set client address if optional host not provided
+if (empty($_GET['host']) && isset($_SERVER['REMOTE_ADDR']) && !\Yggverse\Net\Socket::isHost($_SERVER['REMOTE_ADDR']))
+{
+    $host = $_SERVER['REMOTE_ADDR'];
+}
+
+else
+{
+    $host = $_GET['host'];
+}
+
 // Connection test
 exit(
     json_encode(
         [
-            'success' => \Yggverse\Net\Socket::isOpen($_GET['host'], $_GET['port'])
+            'success' => \Yggverse\Net\Socket::isOpen($_GET['host'], $_GET['port'], 3)
         ]
     )
 );
